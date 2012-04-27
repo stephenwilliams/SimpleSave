5b1348ef52e9964a809e1f56c71d3376f3c0b379
alta189 <me@alta189.com>
2012-04-27 12:43:30 -0400

    Finish IdFactory

--- src/main/java/com/alta189/simplesave/internal/IdFactory.java
+++ src/main/java/com/alta189/simplesave/internal/IdFactory.java
@@ -1,20 +1,24 @@
 package com.alta189.simplesave.internal;
 
 import com.alta189.simplesave.Id;
+import com.alta189.simplesave.exceptions.TableRegistrationException;
 
 import java.lang.reflect.Field;
 
 public class IdFactory {
 
-	public static IdRegistration getId(Class<?> clazz) {
+	public static IdRegistration getId(Class<?> clazz) throws TableRegistrationException {
 		for (Field field : clazz.getDeclaredFields()) {
 			if (!field.isAnnotationPresent(Id.class))
 				continue;
 
 			Class<?> type = field.getType();
+			if (type.equals(int.class))
+				throw new TableRegistrationException("The id is not of type 'int'");
 
+			return new IdRegistration(field.getName(), type);
 		}
-		return null;
+		throw new TableRegistrationException("No field with the @Id annotation");
 	}
 
 }
