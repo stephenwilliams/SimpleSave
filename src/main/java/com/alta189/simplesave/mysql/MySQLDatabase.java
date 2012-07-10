db95956b4c22051e19e182a88d76dde885e1920f
Robert Xu <robxu9@gmail.com>
2012-07-09 21:01:51 -0400

    minour detail
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/alta189/simplesave/mysql/MySQLDatabase.java
+++ src/main/java/com/alta189/simplesave/mysql/MySQLDatabase.java
@@ -121,6 +121,9 @@ public class MySQLDatabase extends Database {
 			try {
 				conn = DriverManager.getConnection(connUrl, user, pass);
 				createTables();
+				for (TableRegistration t : getTableRegistrations()){
+					checkTableStructure(t);
+				}
 			} catch (SQLException e) {
 				throw new ConnectionException(e);
 			}
