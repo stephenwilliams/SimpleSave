07f1aba3966cf468395b54a14fc435b52799e190
alta189 <me@alta189.com>
2012-08-22 15:27:46 -0400

    Removed unused import

--- src/main/java/com/alta189/simplesave/internal/ResultSetUtils.java
+++ src/main/java/com/alta189/simplesave/internal/ResultSetUtils.java
@@ -17,7 +17,6 @@
 package com.alta189.simplesave.internal;
 
 import com.alta189.simplesave.internal.reflection.EmptyInjector;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.h2.util.IOUtils;
 
 import java.io.ByteArrayInputStream;
