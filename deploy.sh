64546a7c73a9889019c93539ef092a1a6d339814
Robert Xu <robxu9@gmail.com>
2014-01-29 11:37:29 -0500

    fix settings.xml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 mkdir -pv ~/.m2
-echo "<server>" > ~/.m2/settings.xml
+cat > ~/.m2/settings.xml << "EOF"
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          http://maven.apache.org/xsd/settings-1.0.0.xsd">
+<servers>
+EOF
+echo "<server>" >> ~/.m2/settings.xml
 echo "<id>bintray-robxu9-SimpleSave-simplesave</id>" >> ~/.m2/settings.xml
 echo "<username>$BINTRAY_USER</username>" >> ~/.m2/settings.xml
 echo "<password>$BINTRAY_PASS</password>" >> ~/.m2/settings.xml
 echo "</server>" >> ~/.m2/settings.xml
-
+cat >> ~/.m2/settings.xml << "EOF"
+</servers>
+</settings>
