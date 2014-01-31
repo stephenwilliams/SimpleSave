d198ca6ec42f74f22e834ea531425e44cbfcd054
Robert Xu <robxu9@gmail.com>
2014-01-30 19:54:48 -0500

    now deploy to jfrog oss (and then to bintray)

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,63 @@
 
 mkdir -pv ~/.m2
 cat > ~/.m2/settings.xml << "EOF"
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          http://maven.apache.org/xsd/settings-1.0.0.xsd">
-<servers>
-EOF
-echo "<server>" >> ~/.m2/settings.xml
-echo "<id>bintray-robxu9-SimpleSave-simplesave</id>" >> ~/.m2/settings.xml
-echo "<username>$BINTRAY_USER</username>" >> ~/.m2/settings.xml
-echo "<password>$BINTRAY_PASS</password>" >> ~/.m2/settings.xml
-echo "</server>" >> ~/.m2/settings.xml
-cat >> ~/.m2/settings.xml << "EOF"
-</servers>
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd" xmlns="http://maven.apache.org/SETTINGS/1.1.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <servers>
+    <server>
+      <username>BINTRAY_USER</username>
+      <password>BINTRAY_PASS</password>
+      <id>central</id>
+    </server>
+    <server>
+      <username>BINTRAY_USER</username>
+      <password>BINTRAY_PASS</password>
+      <id>snapshots</id>
+    </server>
+  </servers>
+  <profiles>
+    <profile>
+      <repositories>
+        <repository>
+          <snapshots>
+            <enabled>false</enabled>
+          </snapshots>
+          <id>central</id>
+          <name>libs-release</name>
+          <url>http://oss.jfrog.org/artifactory/libs-release</url>
+        </repository>
+        <repository>
+          <snapshots />
+          <id>snapshots</id>
+          <name>libs-snapshot</name>
+          <url>http://oss.jfrog.org/artifactory/libs-snapshot</url>
+        </repository>
+      </repositories>
+      <pluginRepositories>
+        <pluginRepository>
+          <snapshots>
+            <enabled>false</enabled>
+          </snapshots>
+          <id>central</id>
+          <name>plugins-release</name>
+          <url>http://oss.jfrog.org/artifactory/plugins-release</url>
+        </pluginRepository>
+        <pluginRepository>
+          <snapshots />
+          <id>snapshots</id>
+          <name>plugins-snapshot</name>
+          <url>http://oss.jfrog.org/artifactory/plugins-snapshot</url>
+        </pluginRepository>
+      </pluginRepositories>
+      <id>artifactory</id>
+    </profile>
+  </profiles>
+  <activeProfiles>
+    <activeProfile>artifactory</activeProfile>
+  </activeProfiles>
 </settings>
+EOF
+
+sed -i s/BINTRAY_USER/$BINTRAY_USER/ ~/.m2/settings.xml
+sed -i s/BINTRAY_PASS/$BINTRAY_PASS/ ~/.m2/settings.xml
